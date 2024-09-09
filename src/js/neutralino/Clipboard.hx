133aa7f875b4b86faf0fb7de2789d21079fe081d
Cédric Belin <cedric@belin.io>
2024-09-09 13:38:43 +0200

    Update the `Clipboard` APIs

--- src/js/neutralino/Clipboard.hx
+++ src/js/neutralino/Clipboard.hx
@@ -1,13 +1,73 @@
 package js.neutralino;
 
+import js.lib.ArrayBuffer;
 import js.lib.Promise;
 
 /** Provides access to the system clipboard. **/
 extern class Clipboard {
 
+	/** Returns the current data format of the system clipboard. **/
+	function getFormat(): Promise<ClipboardFormat>;
+
+	/** Reads an image from the system clipboard. **/
+	function readImage(): Promise<ClipboardImage>;
+
 	/** Reads text from the system clipboard. **/
 	function readText(): Promise<String>;
 
+	/** Writes an image into the system clipboard. **/
+	function writeImage(image: ClipboardImage): Promise<Void>;
+
 	/** Writes text into the system clipboard. **/
-	function writeText(data: String): Promise<Void>;
+	function writeText(text: String): Promise<Void>;
+}
+
+/** The format of the current clipboard data. **/
+enum abstract ClipboardFormat(String) from String to String {
+
+	/** The clipboard contains an image. **/
+	var Image = "image";
+
+	/** The clipboard contains text. **/
+	var Text = "text";
+
+	/** The format of the clipboard data is unknown. **/
+	var Unknown = "unknown";
+}
+
+/** Represents an image stored into the system clipboard. **/
+typedef ClipboardImage = {
+
+	/** The blue mask. **/
+	var ?blueMask: Int;
+
+	/** The blue shift. **/
+	var ?blueShift: Int;
+
+	/** The number of bits per pixel (BPP). **/
+	var ?bpp: Int;
+
+	/** The number of bytes per row (BPR). **/
+	var ?bpr: Int;
+
+	/** The raw RGBA binary data of the image in an array buffer. **/
+	var ?data: ArrayBuffer;
+
+	/** The green mask. **/
+	var ?greenMask: Int;
+
+	/** The green shift. **/
+	var ?greenShift: Int;
+
+	/** The image height. **/
+	var ?height: Int;
+
+	/** The red mask. **/
+	var ?redMask: Int;
+
+	/** The red shift. **/
+	var ?redShift: Int;
+
+	/** The image width. **/
+	var ?width: Int;
 }
