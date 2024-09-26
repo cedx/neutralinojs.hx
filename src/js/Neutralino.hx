afb767a3bc611fa6166ac4fcd11a4b6c3e2e7229
Cédric Belin <cedric.belin@mc2it.com>
2024-09-26 13:45:27 +0200

    Upgraded the Neutralinojs APIs to version 5.4.0

--- src/js/Neutralino.hx
+++ src/js/Neutralino.hx
@@ -9,6 +9,7 @@ import js.neutralino.Events;
 import js.neutralino.Extensions;
 import js.neutralino.FileSystem;
 import js.neutralino.Os;
+import js.neutralino.Resources;
 import js.neutralino.Storage;
 import js.neutralino.Updater;
 import js.neutralino.Window;
@@ -43,6 +44,9 @@ extern class Neutralino {
 
 	/** Access to the user's operating system. **/
 	static final os: Os;
+
+	/** The resource manager. **/
+	static final resources: Resources;
 
 	/** The shared key-value storage. **/
 	static final storage: Storage;
---
+++ src/js/neutralino/Resources.hx
@@ -0,0 +1,20 @@
+package js.neutralino;
+
+import js.lib.ArrayBuffer;
+import js.lib.Promise;
+
+/** Provides methods to parse, read, and extract the loaded resource bundle (`resources.neu`). **/
+extern class Resources {
+
+	/** Extracts a file from the resources bundle to a preferred path. **/
+	function extractFile(path: String, destination: String): Promise<Void>;
+
+	/** Returns all files and directories embedded in the resource bundle. **/
+	function getFiles(): Promise<Array<String>>;
+
+	/** Reads a binary file from the resource bundle. **/
+	function readBinaryFile(path: String): Promise<ArrayBuffer>;
+
+	/** Reads a text file from the resource bundle. **/
+	function readFile(path: String): Promise<String>;
+}
